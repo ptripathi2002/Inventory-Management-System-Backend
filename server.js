0555e58e2e54b71a3537755696febb2001e7a38c
ptripathi2002 <90204816+ptripathi2002@users.noreply.github.com>
2023-03-06 00:54:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", ""],
+    origin: ["http://localhost:3000", "https://inventory-manager-webapp.vercel.app/"],
     credentials: true,
   })
 );
