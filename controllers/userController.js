c27168f45c2c6ee6fc80d1706f049acb3f270e79
Prashant Tripathi <90204816+ptripathi2002@users.noreply.github.com>
2023-03-09 23:57:30 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -254,7 +254,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
 
     //Construct Reset URL
 
-    const resetUrl = `${process.env.FRONTEND_URL}/resetPassword/${resetToken}`;
+    const resetUrl = `${https://inventory-manager-webapp.vercel.app}/resetPassword/${resetToken}`;
 
     //Reset Email
     const message = `
